2a48cfefafa356312d946000c1c2755da0e3b930
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-08-27 18:53:44 -0300

    Create .h

---
+++ usb cam/enable/disable autofocus/.h
@@ -0,0 +1,43 @@
+#ifndef USB_CAM_USB_CAM_H
+#define USB_CAM_USB_CAM_H
+
+#include <asm/types.h>          /* for videodev2.h */
+
+extern "C"
+{
+#include <linux/videodev2.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include <libavutil/mem.h>
+}
+
+// legacy reasons
+#include <libavcodec/version.h>
+#if LIBAVCODEC_VERSION_MAJOR < 55
+#define AV_CODEC_ID_MJPEG CODEC_ID_MJPEG
+#endif
+
+#include <string>
+#include <sstream>
+
+#include <sensor_msgs/Image.h>
+
+namespace usb_cam {
+
+class UsbCam {
+ public:
+  typedef enum
+  {
+    IO_METHOD_READ, IO_METHOD_MMAP, IO_METHOD_USERPTR, IO_METHOD_UNKNOWN,
+  } io_method;
+
+  typedef enum
+  {
+    PIXEL_FORMAT_YUYV, PIXEL_FORMAT_UYVY, PIXEL_FORMAT_MJPEG, PIXEL_FORMAT_YUVMONO10, PIXEL_FORMAT_RGB24, PIXEL_FORMAT_GREY, PIXEL_FORMAT_UNKNOWN
+  } pixel_format;
+
+  UsbCam();
+  ~UsbCam();
+
+// enable/disable auto focus
+void set_auto_focus(int value);
