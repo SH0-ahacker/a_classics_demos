e83ff7b927d57c3cd5dd568e0929d7c431f8926a
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-08-28 22:11:51 -0300

    Create .py

---
+++ chat2/IP/.py
@@ -0,0 +1,7 @@
+/* Both the server and client script can then be run
+   from the Command prompt (in Windows) or from bash 
+   Terminal (Linux users) by simply typing 
+   "python chat_server.py  " or "python client.py  ". 
+   For example, */
+python chat_server.py 192.168.55.13 8081
+python client.py 192.168.55.13 8081
