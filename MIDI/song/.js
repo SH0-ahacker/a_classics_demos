febfb5188619618830bb9ee088512c7a16c7d536
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-08-31 21:31:05 -0300

    Create .js

---
+++ MIDI/song/.js
@@ -0,0 +1,9 @@
+var synth = new Tone.FMSynth().toMaster()
+
+//schedule a series of notes to play as soon as the page loads
+synth.triggerAttackRelease('C4', '4n', '8n')
+synth.triggerAttackRelease('E4', '8n', Tone.Time('4n') + Tone.Time('8n'))
+synth.triggerAttackRelease('G4', '16n', '2n')
+synth.triggerAttackRelease('B4', '16n', Tone.Time('2n') + Tone.Time('8t'))
+synth.triggerAttackRelease('G4', '16', Tone.Time('2n') + Tone.Time('8t')*2)
+synth.triggerAttackRelease('E4', '2n', '0:3')
